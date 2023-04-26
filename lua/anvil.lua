dd5097a6e48dcfffc5edd498e8954e0a9036e1fe
Mr.PAPI <thesnakewitcher@gmail.com>
2023-04-26 11:23:22 -0400

    add: config options

--- lua/anvil.lua
+++ lua/anvil.lua
@@ -4,10 +4,12 @@ local anvil = {}
 
 local defaults = {
     config_file_name = "anvil_conf.json",
-    accounts = 10,
-    balance = 10000,
+    accounts = 15,                          -- number of dev accounts to generate and configure
+    balance = 10000,                        -- balance of every dev account
     host = "127.0.0.1",
     port = "8545",
+    tracing = true,
+    timeout = 45000,                        -- timeout in ms for request to sent remote JSON-RPC
 }
 
 function anvil.setup(options)
@@ -22,7 +24,11 @@ function anvil.spawn()
     Job:new({
         command = 'anvil',
         args = {
-            "--config-out",anvil.options.config_file_name
+            "--config-out",anvil.options.config_file_name,
+            "--host",anvil.options.host,
+            "--port",anvil.options.port,
+            "--accounts",anvil.options.accounts,
+            "--balance",anvil.options.balance,
         },
     }):start()
 end
