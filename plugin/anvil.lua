48f1f5a3e864b63ab9d8bba3198385bf60e1c192
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-28 21:03:48 -0400

    ref: delete unneded plugin/anvil.lua module

--- plugin/anvil.lua
+++
@@ -1,9 +0,0 @@
-if vim.fn.has("nvim-0.8.0") == 0 then
-  vim.notify("anvil requires at least nvim-0.8.0.1")
-  return
-end
-
-if vim.g.loaded_anvil == true then
-  return
-end
-vim.g.loaded_anvil = true
