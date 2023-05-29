88999b71a7a3bd1c817828240c6062d3f52b2b81
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-28 21:05:05 -0400

    ref: api and add events

--- lua/anvil/init.lua
+++ lua/anvil/init.lua
@@ -1,5 +1,5 @@
 local Job = require("plenary.job")
-local util = require("chisel.util")
+local util = require("anvil.util")
 
 
 local M = {
@@ -9,8 +9,8 @@ local M = {
 
 local default_config = {
     config_file_name = "anvil_conf.json",   -- file where to output anvil config when spawned 
-    accounts = 10,                          -- number of dev accounts to generate and configure
-    balance = 10000,                        -- balance of every dev account
+    accounts = 10,                          -- number of accounts to generate and configure
+    balance = 10000,                        -- balance of every account
     host = "127.0.0.1",
     port = "8545",
     tracing = true,
@@ -23,6 +23,8 @@ end
 
 -- optional: fork_url , fork_block_number , fork_chain_id
 function M.start(opts)
+    vim.api.nvim_exec_autocmds("User",{pattern = "AnvilStartPre"})
+
     local args_tbl = {
         "--config-out",M.config.config_file_name,
         "--host",M.config.host,
@@ -30,13 +32,16 @@ function M.start(opts)
         "--accounts",M.config.accounts,
         "--balance",M.config.balance,
     }
-    args_tbl = util.insert_opts(opts,args_tbl)
+    args_tbl = util.insert_not_nil_opts(opts,args_tbl)
 
     Job:new({
         command = "anvil",
         args = args_tbl,
     }):start()
     M.is_running = true
+    vim.notify("anvil started")
+
+    vim.api.nvim_exec_autocmds("User",{pattern = "AnvilStartPost", data = {options = args_tbl} })
 end
 
 local function get_pid()
@@ -45,6 +50,8 @@ local function get_pid()
 end
 
 function M.stop()
+    vim.api.nvim_exec_autocmds("User",{pattern = "AnvilStopPre"})
+
     local anvil_pid = get_pid()
     if not anvil_pid then
         vim.notify("anvil not runnig")
@@ -56,43 +63,62 @@ function M.stop()
         args = { anvil_pid },
     }):start()
     M.is_running = false
+    vim.notify("anvil stoped")
 
+    vim.api.nvim_exec_autocmds("User",{pattern = "AnvilStopPost"})
 end
 
-function M.get_state()
+
+local function get_state()
     if not M.is_running then return nil end
     local file = io.open(M.config.config_file_name,"r")
     local file_content = file:read("*a")
     M.state = vim.json.decode(file_content)
 end
 
-function M.get_accounts(index)
+local function check_state()
+    if not vim.tbl_isempty(M.state) then
+        return true
+    end
+
+    get_state()
 
     if vim.tbl_isempty(M.state) then
-        M.get_state()
-    end
-    if vim.tbl_isempty(M.state) then return nil end
-
-
-    if not index then
-        return M.state.accounts
+        return false
     else
-        return M.state.accounts[index]
+        return true
     end
-
 end
 
-function M.get_account_index(account_address)
+function M.get_accounts(index)
+    if not check_state() then return nil end
+    if not index then
+        return M.state.available_accounts
+    else
+        return M.state.available_accounts[index]
+    end
+end
+
+
+local function get_account_index(account_address)
+    if not account_address then return nil end
+
+    if type(account_address) == "number" then
+        return account_address
+    end
+
     for index,account in ipairs(M.state.accounts) do
         if account == account_address then
             return index
         end
     end
+
     return nil
 end
 
-function M.get_private_key(account_address)
-    local index = M.get_account_index(account_address)
+function M.get_private_key(account)
+    local index = get_account_index(account)
+
     if index then
         return M.state.private_keys[index]
     else
